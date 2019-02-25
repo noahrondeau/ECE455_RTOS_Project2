f2d4b5142938e3ab76721b3f42609d97b1aea931
Matthew Wegener <mwegener@protonmail.com>
2019-02-25 11:38:56 -0800

    Changed to scale traffic light duration using a float

--- ECE455_RTOS_Project2/src/TrafficLight.c
+++ ECE455_RTOS_Project2/src/TrafficLight.c
@@ -19,70 +19,76 @@ void vTrafficLightInit(TrafficLight_t* trafficLight)
 	trafficLight->init = true;
 }
 
+/****NOMINAL OPERATION******
+ * Receives flow from generator.
+ * On first system run light will be initially green,
+ * each sequential run will transition the light.
+ * Duration each light is on scaled from the base delay of the yellow light (1s)
+ * Nominal length for green and red is 4s under no load
+ * Light length is scaled using the float
+ */
 void vTrafficLightControlTask(void* pvParameters)
 {
-	//Parameter passed in from traffic flow task
-	int trafficLoad;
-
+	//Parameter obtained from traffic flow task
+	float trafficFlow;
 
 	while(1)
 	{
 
 		if (xSemaphoreTake(xFlowMutex, (portTickType)10) == pdTRUE )
 		{
-			trafficLoad = flowRate;
+			trafficFlow = flowRate;
 			xSemaphoreGive(xFlowMutex);
 		}
 
-		// See if we can obtain the semaphore.  If the semaphore is not available
-		// wait 10 ticks to see if it becomes free.
+		// See if we can obtain the semaphore.  If the semaphore is not available wait 10 ticks to see if it becomes free.
 		if( xSemaphoreTake( xLightMutex, ( portTickType ) 10 ) == pdTRUE )
 		{
 
 			/*
-			If first time then state should be in green
-			otherwise update currentState from previous time task running
+			If first time task is run the state should be in green
+			otherwise update currentState from previous time task ran
 			 */
 			if(trafficLight.init) trafficLight.init=false;
-			else trafficLight.currentState = trafficLight.nextState;
+				else trafficLight.currentState = trafficLight.nextState;
 
 
-			//sets task delays based on load and current state
+			//sets task delays based on received load and current state
 			switch (trafficLight.currentState)
 			{
 			case Red:
-				trafficLight.nextState = Green;
-
 				//if heavy traffic have a short red (3s) other wise have a long red (5s)
-				if(trafficLoad) trafficLight.lightDelay = 3*trafficLight.baseDelay;
-				else trafficLight.lightDelay = 5*trafficLight.baseDelay;
+				trafficLight.nextState = Green;
+				trafficLight.lightDelay = 4*trafficLight.baseDelay - (int)(trafficFlow*1000);
 				break;
 
 			case Yellow:
+
 				trafficLight.nextState = Red;
-				trafficLight.lightDelay = trafficLight.baseDelay;
+				trafficLight.lightDelay = (trafficLight.baseDelay);
+
 				break;
 
 			case Green:
-				trafficLight.nextState = Yellow;
-
 				//if heavy traffic have a long green (8s) other wise have a short green (4s)
-				if(trafficLoad) trafficLight.lightDelay = 8*trafficLight.baseDelay;
-				else trafficLight.lightDelay = 4*trafficLight.baseDelay;
+				trafficLight.nextState = Yellow;
+				trafficLight.lightDelay = 4*trafficLight.baseDelay + (int)(trafficFlow*4000);
 				break;
 
 			default:
+				//should never reach here
+				printf("light state error");
 				break;
 			}
 
-			xSemaphoreGive( xLightMutex);
+			xSemaphoreGive(xLightMutex);
 
+			//Delay by calculated amount dependent on light and conditions
 			vTaskDelay(trafficLight.lightDelay);
 		}
 		else
 		{
-			// We could not obtain the semaphore and can therefore not access
-			// the shared resource safely.
+			// We could not obtain the semaphore therefore delay by base amount
 			vTaskDelay(trafficLight.baseDelay);
 		}
 	}
